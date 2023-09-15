d7d131395db934a1b8f8f599459199330f9f5637
FTSadori <suzium.trap@gmail.com>
2023-09-15 18:40:24 +0300

    Add lab 2 files

---
+++ Lab2/lab_2_Romenskyi.py
@@ -0,0 +1,25 @@
+# Task 1
+print("Programming", "Essential", "in", sep="***", end="...")
+print("Python")
+
+# Task 2
+print("    *    ")
+print("   * *   ")
+print("  *   *  ")
+print(" *     * ")
+print("***   ***")
+print("  *   *  ")
+print("  *   *  ")
+print("  *****  ")
+
+# Task 3
+print("I'm student")
+
+# Task 4
+print('"I\'m"\n""learning""\n"""Python"""')
+
+# Task 5
+print(500_8 == 5008)
+
+# Task 6
+print(0x777)
